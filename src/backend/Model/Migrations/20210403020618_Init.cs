84f7ad44b3232645807d0ee39fbf1d4ba71161a3
hirasaki-t <dg1.048596@outlook.jp>
2021-04-03 11:07:45 +0900

    migration発行

---
+++ src/backend/Model/Migrations/20210403020618_Init.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Model.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authorities",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authorities", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IssueStatuses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IssueStatuses", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectStatuses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectStatuses", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentID = table.Column<int>(type: "int", nullable: false),
+                    AuthorityID = table.Column<int>(type: "int", nullable: false),
+                    LoginID = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Neme = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mail = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Users_Authorities_AuthorityID",
+                        column: x => x.AuthorityID,
+                        principalTable: "Authorities",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Users_Departments_DepartmentID",
+                        column: x => x.DepartmentID,
+                        principalTable: "Departments",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProjectStatusID = table.Column<int>(type: "int", nullable: false),
+                    DepartmentID = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Projects_Departments_DepartmentID",
+                        column: x => x.DepartmentID,
+                        principalTable: "Departments",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Projects_ProjectStatuses_ProjectStatusID",
+                        column: x => x.ProjectStatusID,
+                        principalTable: "ProjectStatuses",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Issues",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProjectID = table.Column<int>(type: "int", nullable: false),
+                    IssueStatusID = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Update = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Issues", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Issues_IssueStatuses_IssueStatusID",
+                        column: x => x.IssueStatusID,
+                        principalTable: "IssueStatuses",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Issues_Projects_ProjectID",
+                        column: x => x.ProjectID,
+                        principalTable: "Projects",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IssueID = table.Column<int>(type: "int", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    PostMessage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Messages_Issues_IssueID",
+                        column: x => x.IssueID,
+                        principalTable: "Issues",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Authorities",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "管理者" },
+                    { 2, "一般ユーザー" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "開発" },
+                    { 2, "営業" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "IssueStatuses",
+                columns: new[] { "ID", "Status" },
+                values: new object[,]
+                {
+                    { 1, "未解決" },
+                    { 2, "解決" },
+                    { 3, "保留" },
+                    { 4, "取下" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProjectStatuses",
+                columns: new[] { "ID", "Status" },
+                values: new object[,]
+                {
+                    { 8, "ユーザーテスト" },
+                    { 7, "内部テスト" },
+                    { 6, "開発" },
+                    { 5, "詳細設計" },
+                    { 1, "未着手" },
+                    { 3, "要件定義" },
+                    { 2, "要求分析" },
+                    { 9, "リリース準備" },
+                    { 4, "基本設計" },
+                    { 10, "リリース" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_IssueStatusID",
+                table: "Issues",
+                column: "IssueStatusID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_ProjectID",
+                table: "Issues",
+                column: "ProjectID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_IssueID",
+                table: "Messages",
+                column: "IssueID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_UserID",
+                table: "Messages",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_DepartmentID",
+                table: "Projects",
+                column: "DepartmentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_ProjectStatusID",
+                table: "Projects",
+                column: "ProjectStatusID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_AuthorityID",
+                table: "Users",
+                column: "AuthorityID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_DepartmentID",
+                table: "Users",
+                column: "DepartmentID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Issues");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "IssueStatuses");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Authorities");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "ProjectStatuses");
+        }
+    }
+}
